d6ab96d0f006c0c21cb17910e59b44db52a3c480
kibriaji5-pixel <kibriaji5@gmail.com>
2026-04-12 16:37:03 +0600

    Update pt2.py

--- onno sir/pt2.py
+++ onno sir/pt2.py
@@ -1,1 +1,40 @@
+# i=1
+# total = 0
 
+# while i <= 5:
+#     total+= i
+#     i += 1
+
+#     print("Total: ", total)
+
+
+# name= " "
+
+# while name == " ":
+#     name = input("Enter your name: ")
+# print("hello", name)
+# for num in range(10):
+#     if num == 5:
+#         continue
+#     print(num)
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# even = [n for n in numbers if n % 2 ==0]
+# print(even) 
+# def status(*numbers, **options):
+#     operation = options.get("operation", "sum")
+#     result = None
+    
+#     if operation == "sum":
+#         result = sum(numbers)
+#     elif operation == "avg":
+#         result = sum(numbers) / len(numbers)
+#     elif operation == "max":
+#         result = max(numbers)
+    
+#     return result
+
+
+# print(status(10, 20, 30, operation="sum"))
+# print(status(10, 20, 30, operation="avg"))
+# print(status(10, 20, 30, operation="max"))# eitar gulao bujhini,,
