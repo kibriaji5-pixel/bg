e1a7e3098b5bba47a28c5bb1aa3e71925d0afec2
kibriaji5-pixel <kibriaji5@gmail.com>
2026-04-12 14:31:00 +0600

    Create pt.py

---
+++ onno sir/pt.py
@@ -0,0 +1,11 @@
+# for i in range(1,101):
+#     if i%2==0:
+#         print(i)
+
+
+fruits=["aam","jam","kola","lechu","anarosh","apel"]
+newlist=[]
+for x in fruits:
+    if "a" in x:
+        newlist.append(x)
+print(newlist)
